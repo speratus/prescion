13d15789d7146b4db312e346981231cf940d2942
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-22 09:25:34 -0500

    generates series model

---
+++ db/migrate/20210122142437_create_series.rb
@@ -0,0 +1,11 @@
+class CreateSeries < ActiveRecord::Migration[6.1]
+  def change
+    create_table :series do |t|
+      t.string :name
+      t.text :description
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
