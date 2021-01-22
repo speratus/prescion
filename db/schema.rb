455f8396fc08731e79d76bdff138e2e317e1e4d7
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-22 09:35:58 -0500

    generates tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_142056) do
+ActiveRecord::Schema.define(version: 2021_01_22_143526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_01_22_142056) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "series", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_series_on_user_id"
+  end
+
   create_table "stories", force: :cascade do |t|
     t.string "title"
     t.datetime "publication_date"
@@ -52,6 +61,12 @@ ActiveRecord::Schema.define(version: 2021_01_22_142056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_stories_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,5 +81,6 @@ ActiveRecord::Schema.define(version: 2021_01_22_142056) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "series", "users"
   add_foreign_key "stories", "users"
 end
