05c9b868e8bb36b2f40ec9da7624a10e9ad29e74
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-22 20:43:05 -0500

    updates fixtures for attribute tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "User has attributes" do
+    u = users(:one)
+    assert_equal "Test guy", u.name
+
+    assert_equal "iamtest@universe.test.space", u.email
+    assert_equal "testguy", u.username
+    assert u.authenticate('hello world')
+    assert_not_nil u.bio
+  end
 end
