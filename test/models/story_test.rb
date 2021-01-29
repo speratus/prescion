a34d84dddb219164505a41bf8fd9ce3a5faf1876
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-29 11:39:45 -0500

    adds test for tag association

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -4,4 +4,16 @@ class StoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "stories can have many tags" do
+    s = stories(:one)
+
+    assert_equal 2, s.tags.count
+
+    tag_names = s.tags.map do |t|
+      t.name
+    end
+
+    assert_equal ['advice', 'tricks'], tag_names
+  end
 end
