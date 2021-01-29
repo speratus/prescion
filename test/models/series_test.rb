9642561b7835d801ba05556bab9ae2f014baecdd
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-29 11:43:07 -0500

    adds test for series tags

--- test/models/series_test.rb
+++ test/models/series_test.rb
@@ -4,4 +4,16 @@ class SeriesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Series can have many tags" do
+    s = series(:one)
+
+    assert_equal 2, s.tags.count
+
+    tag_names = s.tags.map do |t|
+      t.name
+    end
+
+    assert_equal ['life hacks', 'advice'], tag_names
+  end
 end
