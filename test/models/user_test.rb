2b68a39094de24bcdb1735854017f7ac2adf362d
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-29 11:20:22 -0500

    adds tests for user associations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,14 @@ class UserTest < ActiveSupport::TestCase
     assert u.authenticate('hello world')
     assert_not_nil u.bio
   end
+
+  test "User has more than 0 stories" do
+    u = users(:one)
+
+    assert u.stories.count > 0
+  end
+
+  test "User has more than 0 series" do
+    assert users(:one).series.count > 0
+  end
 end
