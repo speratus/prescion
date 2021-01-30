bfab7c5feaf2fd2c468a40b717b38c7937b81522
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-01-29 23:11:50 -0500

    adds basic login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :x_render
+
+    def x_render(variable, default)
+        local_variables.include?(variable) ? local_variable_get(variable) : default
+    end
 end
