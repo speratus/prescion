ed3584bf80d3b8a35705584403cfdc0693e976d9
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-02-19 10:36:07 -0500

    adds login functionality

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,25 @@ class LoginController < ApplicationController
   def login
   end
 
+  def create_session
+    user = User.find_by(username: login_params[:username])
+
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      redirect_to login_path
+    end
+  end
+
   def signup
     @user = User.new
   end
+
+  private
+
+  def login_params
+    params.require([:username, :password])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'fonts/*ttf', to: 'fonts#materialdesignicons_webfont'
   get 'login', to: 'login#login', as: 'login'
   get 'signup', to: 'login#signup', as: 'signup'
+  post 'create-session', to: 'login#create_session', as: 'create_session'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
 end
