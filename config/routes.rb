3e803025d42cb6f430e1573bbe02c10e648868b9
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-02-19 10:30:09 -0500

    makes login and signup routes more user friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'fonts/*ttf', to: 'fonts#materialdesignicons_webfont'
-  get 'login/login'
-  get 'login/signup'
+  get 'login', to: 'login#login', as: 'login'
+  get 'signup', to: 'login#signup', as: 'signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
 end
